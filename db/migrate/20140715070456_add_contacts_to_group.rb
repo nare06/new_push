ba32b5ceb7301c9fa29bdf3ca160cc2d2d8575f9
nare06 <nare06@gmail.com>
2014-07-31 11:00:30 +0000

    Delete 20140715070456_add_contacts_to_group.rb

--- db/migrate/20140715070456_add_contacts_to_group.rb
+++
@@ -1,7 +0,0 @@
-class AddContactsToGroup < ActiveRecord::Migration
-  def change
-      add_column :groups, :contact_name, :string
-      add_column :groups, :contact_phone, :integer 
-      add_column :groups, :email, :string
-  end
-end
